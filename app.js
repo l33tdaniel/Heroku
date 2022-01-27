f6405164bf47bcae517ba4a273fc3144f10001ed
danielrockhurst <danielneugent@amdg.rockhursths.edu>
2022-01-27 12:43:02 -0600

    Init

---
+++ app.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.resolve(__dirname, 'client/build')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+// Test api connection
+app.get("/api", (req, res) => {
+    res.json({ message: "Hello from server!" });
+});
+
+// Handles any requests that don't match the ones above
+app.get('*', (req, res) =>{
+    res.sendFile(path.resolve(__dirname, 'client/build/index.html'));
+});
+
+module.exports = app;
