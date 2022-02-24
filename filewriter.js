1a53352dbae6592ec36c7e2e57308ce1d5524073
danielrockhurst <danielneugent@amdg.rockhursths.edu>
2022-02-24 12:49:47 -0600

    wrote to file

--- filewriter.js
+++ filewriter.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
-
+// this works!
 let lyrics = 'But still I\'m having memories of high speeds when the cops crashed\n' + 
              'As I laugh, pushin the gas while my Glocks blast\n' + 
              'We was young and we was dumb but we had heart';
